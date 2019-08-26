eca35a6ca48fd69a38196b16b12061a2b0f58db8
mgrankin <mv.grankin@gmail.com>
2019-08-26 20:53:22 +0300

    Revert "start of refactoring/renaming"
    This reverts commit 2f26bbc29870798324f52ac0bbf24c75e9ff561c.

--- over9000.py
+++ over9000.py
@@ -1,16 +1,136 @@
 import torch, math
 from torch.optim.optimizer import Optimizer
 import itertools as it
-from lookahead import *
-from ralamb import * 
-
-# RAdam + LARS + LookAHead
 
 # Lookahead implementation from https://github.com/lonePatient/lookahead_pytorch/blob/master/optimizer.py
 # RAdam + LARS implementation from https://gist.github.com/redknightlois/c4023d393eb8f92bb44b2ab582d7ec20
 
-def Over9000(params, alpha=0.5, k=6, *args, **kwargs):
-     ralamb = Ralamb(params, *args, **kwargs)
-     return Lookahead(ralamb, alpha, k)
+# RAdam + LARS + LookAHead
+class Over9000(Optimizer):
 
-RangerLars = Over9000
+    def __init__(self, params, lr=1e-3, alpha=0.5, k=6, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
+        #parameter checks
+        if not 0.0 <= alpha <= 1.0:
+            raise ValueError(f'Invalid slow update rate: {alpha}')
+        if not 1 <= k:
+            raise ValueError(f'Invalid lookahead steps: {k}')
+        if not lr > 0:
+            raise ValueError(f'Invalid Learning Rate: {lr}')
+        if not eps > 0:
+            raise ValueError(f'Invalid eps: {eps}')
+ 
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        self.buffer = [[None, None, None] for ind in range(10)]
+        super(Over9000, self).__init__(params, defaults)
+ 
+        # look ahear params
+        for group in self.param_groups:
+            group["step_counter"] = 0
+
+        self.alpha = alpha
+        self.k = k 
+
+        #lookahead weights
+        self.slow_weights = [[p.clone().detach() for p in group['params']]
+                                for group in self.param_groups]
+        
+        #don't use grad for lookahead weights
+        for w in it.chain(*self.slow_weights):
+            w.requires_grad = False
+
+
+    def __setstate__(self, state):
+        super(Over9000, self).__setstate__(state)
+
+    def step(self, closure=None):
+
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data.float()
+                if grad.is_sparse:
+                    raise RuntimeError('Ralamb does not support sparse gradients')
+
+                p_data_fp32 = p.data.float()
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p_data_fp32)
+                    state['exp_avg_sq'] = torch.zeros_like(p_data_fp32)
+                else:
+                    state['exp_avg'] = state['exp_avg'].type_as(p_data_fp32)
+                    state['exp_avg_sq'] = state['exp_avg_sq'].type_as(p_data_fp32)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                # Decay the first and second moment running average coefficient
+                # m_t
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                # v_t
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+
+                state['step'] += 1
+                buffered = self.buffer[int(state['step'] % 10)]
+
+                if state['step'] == buffered[0]:
+                    N_sma, radam_step = buffered[1], buffered[2]
+                else:
+                    buffered[0] = state['step']
+                    beta2_t = beta2 ** state['step']
+                    N_sma_max = 2 / (1 - beta2) - 1
+                    N_sma = N_sma_max - 2 * state['step'] * beta2_t / (1 - beta2_t)
+                    buffered[1] = N_sma
+
+                    # more conservative since it's an approximated value
+                    if N_sma >= 5:
+                        radam_step = group['lr'] * math.sqrt((1 - beta2_t) * (N_sma - 4) / (N_sma_max - 4) * (N_sma - 2) / N_sma * N_sma_max / (N_sma_max - 2)) / (1 - beta1 ** state['step'])
+                    else:
+                        radam_step = group['lr'] / (1 - beta1 ** state['step'])
+                    buffered[2] = radam_step
+
+                if group['weight_decay'] != 0:
+                    p_data_fp32.add_(-group['weight_decay'] * group['lr'], p_data_fp32)
+
+                weight_norm = p.data.pow(2).sum().sqrt().clamp(0, 10)
+                radam_norm = p_data_fp32.pow(2).sum().sqrt()
+                if weight_norm == 0 or radam_norm == 0:
+                    trust_ratio = 1
+                else:
+                    trust_ratio = weight_norm / radam_norm
+
+                state['weight_norm'] = weight_norm
+                state['adam_norm'] = radam_norm
+                state['trust_ratio'] = trust_ratio
+
+                # more conservative since it's an approximated value
+                if N_sma >= 5:
+                    denom = exp_avg_sq.sqrt().add_(group['eps'])
+                    p_data_fp32.addcdiv_(-radam_step * trust_ratio, exp_avg, denom)
+                else:
+                    p_data_fp32.add_(-radam_step * trust_ratio, exp_avg)
+
+                p.data.copy_(p_data_fp32)
+
+        #---------------- end radam step
+        
+        #look ahead tracking and updating if latest batch = k
+        for group,slow_weights in zip(self.param_groups,self.slow_weights):
+            group['step_counter'] += 1
+            if group['step_counter'] % self.k != 0:
+                continue
+            for p,q in zip(group['params'],slow_weights):
+                if p.grad is None:
+                    continue
+                q.data.add_(self.alpha,p.data - q.data)
+                p.data.copy_(q.data)
+
+        return loss
